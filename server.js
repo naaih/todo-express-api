25e001b69383b8f5ec9f8263942d4b9e1562eeb1
naaih <a.naaih@outlook.com>
2023-12-18 18:17:17 +0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// data
+const todos = [
+  {
+    title: "Todo 1",
+    desc: "This is my first Todo",
+    completed: true,
+  },
+
+  {
+    title: "Todo 2",
+    desc: "This is my second Todo",
+    completed: true,
+  },
+
+  {
+    title: "Todo 3",
+    desc: "This is my third Todo",
+    completed: true,
+  },
+
+  {
+    title: "Todo 4",
+    desc: "This is my fourth Todo",
+    completed: true,
+  },
+
+  {
+    title: "Todo 5",
+    desc: "This is my fifth Todo",
+    completed: true,
+  },
+];
+
+// endpoints
+app.get("/", (request, response) => {
+  response.status(200).json(todos);
+});
+
+app.get("/todos", (request, response) => {
+  response.status(200).json(todos);
+});
+
+app.get("/todos/:id", (request, response) => {
+  response
+    .status(200)
+    .json({ data: todos.find((todo) => todo.id === request.params.id) });
+});
+
+app.post("/todos", (request, response) => {
+  todos.push(request.body);
+  response.status(201).json({ msg: "Todo created successfully" });
+});
+
+app.put("/todos/:id", (request, response) => {
+  const todo = todos.find((todo) => todo.id === request.params.id);
+  if (todo) {
+    const { title, desc, completed } = request.body;
+    todo.title = title;
+    todo.desc = desc;
+    todo.completed = completed;
+    response.status(200).json({ msg: "Todo updated sucessfully" });
+    return;
+  }
+  response.status(404).json({ msg: "Todo not found" });
+});
+
+app.delete("/todos/:id", (request, response) => {
+  const todoIndex = todos.findIndex((todo) => (todo.id = request.params.id));
+  if (todoIndex) {
+    todos.splice(todoIndex, 1);
+    response.status(200).json({ msg: "Todo deleted successfully" });
+  }
+  response.status(404).json({ msg: "Todo not found" });
+});
+
+// app gets incoming requests
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
